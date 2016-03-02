27792473fdfa6efbd15371b5b549b531963e2b48
Balaji <rbalajives@gmail.com>
2016-03-02 13:44:36 +0530

    completed

--- generate.py
+++ generate.py
@@ -4,13 +4,11 @@ print "..............................."
 file1 = "url.txt"
 name = 0
 file2 = open(file1,"r")
-while file2.readline() :
-	b = file2.readline()
+g = file2.readlines()
+for h in g:
 	name = name + 1
 	i = str(name)
-	a = i + "file"
-	file3 = open(a, "w")
-	file3.write(b)
-	file3.close()
+	a = i + ".pdf"
+	pdfkit.from_url(h,a)
 file2.close()
 print "%d"%name+"files Completed"
